29eb0c055ffaad4d03cf3ee7f4e090b0fe304748
Linkka <samilindroos@hotmail.com>
2016-01-26 00:43:50 +0200

    Initial commit.

---
+++ Kotitehtava6olio/Dog.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kotitehtava6olio
+{
+    class Dog
+    {
+        // properties
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public int Age { get; set; }
+
+        // don't create any constructor, so default one will be used
+
+        // method to give more speed
+        public void GetOlder()
+        {
+            Age += 1;
+        }
+
+        // method to display car data
+        public void PrintData()
+        {
+            Console.WriteLine("Dog information : ");
+            Console.WriteLine("- Name : " + Name);
+            Console.WriteLine("- Breed : " + Breed);
+            Console.WriteLine("- Age : " + Age);
+        }
+
+        // destructor
+        ~Dog()
+        {
+            Console.WriteLine("Dog object destroyed.");
+        }
+
+    }
+}
---
+++ Kotitehtava6olio/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kotitehtava6olio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // luo opiskelijat
+            Dog dog1 = new Dog();
+            dog1.Name = "Cookie";
+            dog1.Breed = "Huskymix";
+            dog1.Age = 0;
+
+            Dog dog2 = new Dog();
+            dog2.Name = "Vilma";
+            dog2.Breed = "Mix";
+            dog2.Age = 5;
+
+            // display car data
+            dog1.PrintData();
+            dog2.PrintData();
+            Console.ReadLine();
+            // accelerate, change car's speed value
+            dog1.GetOlder();
+            dog2.GetOlder();
+            // display car data again, notice changed speed value!
+            dog1.PrintData();
+            dog2.PrintData();
+            Console.ReadLine();
+        }
+    }
+}
